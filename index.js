22fd49b0cc74d66ec979c57b12389581b85d3653
talal <ta440237@gmail.com>
2023-06-03 03:12:46 +0300

    solve assignment

--- index.js
+++ index.js
@@ -46,3 +46,51 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+// index.js
+
+const [moo, neigh, baa, oink, cluck] = ["cow", "horse", "sheep", "pig", "chicken"];
+const { bessie, dolly, babe, little } = { bessie: "cow", dolly: "sheep", babe: "pig", little: "chicken" };
+const { blackAndWhite, black, pink } = { blackAndWhite: "cow", black: "sheep", pink: "pig" };
+const [red, orange, yellow, green, blue, indigo, violet] = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const [r, o, y, g, b, , v] = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const [, , , , , indg] = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const { muppetName, color, song, job, partner } = { muppetName: "Miss Piggy", color: "pink", song: "Never Before, Never Again", job: "Cast member of The Muppet Show", partner: "Kermit" };
+const { song2, song4, nested: { job: nestedJob, partner: nestedPartner } } = { song2: "Moving Right Along", song4: "I Hope That Something Better Comes Along", nested: { job: "Host of The Muppet Show", partner: "Miss Piggy" } };
+
+module.exports = {
+  moo,
+  neigh,
+  baa,
+  oink,
+  cluck,
+  bessie,
+  dolly,
+  babe,
+  little,
+  blackAndWhite,
+  black,
+  pink,
+  red,
+  orange,
+  yellow,
+  green,
+  blue,
+  indigo,
+  violet,
+  r,
+  o,
+  y,
+  g,
+  b,
+  v,
+  indg,
+  muppetName,
+  color,
+  song,
+  job,
+  partner,
+  song2,
+  song4,
+  nestedJob,
+  nestedPartner
+};
